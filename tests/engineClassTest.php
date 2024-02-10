2b3eda63261b4615186269f7dece48b3c7522edd
Charlie Hills <github.cb25@tmx.email>
2024-02-10 15:01:58 -0600

    Introduce a pseudo-unit test for ut05
    This a large and complex task, really more functional testing than unit
    testing, so I haven't really settled in on an approach. For now, it
    runs two tests: 1) repeat until fixed period reach, 2) repeat until
    assets depleted.
    
    The only tests are to count the number of rows returned in the plan. But
    it also calls the new Reporting class to dump out CSV and also dumping
    out logs. This will definitely change as it matures. Just a first step.

---
+++ tests/engineClassTest.php
@@ -0,0 +1,65 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+use App\Service\Engine\Engine;
+use App\Service\Engine\Until;
+use App\Service\Reporting\Report;
+
+final class engineClassTest extends TestCase
+{
+    private static Engine $engine;
+    private static Until $until;
+
+    /**
+     * @throws Exception
+     */
+    public function testRunUntil300(): void
+    {
+        self::$engine = new Engine('ut05-expenses', 'ut05-assets', 'ut05-earnings');
+        self::$until = new Until();
+
+        self::$until->setPeriods(300);
+
+        self::$engine->run(self::$until);
+
+        $plan = $this->processOutput();
+
+        $this->assertCount(300, $plan);
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function testRunUntilEmpty(): void
+    {
+        self::$engine = new Engine('ut05-expenses', 'ut05-assets', 'ut05-earnings');
+        self::$until = new Until();
+
+        self::$until->setUntil(Until::ASSETS_DEPLETE);
+
+        self::$engine->run(self::$until);
+
+        $plan = $this->processOutput();
+
+        $this->assertCount(6, $plan);
+    }
+
+    private function processOutput(): array
+    {
+        $plan = self::$engine->getPlan();
+        $logs = self::$engine->getLogs();
+
+        $reporting = new Report();
+        $report = $reporting->standard($plan);
+
+        $csvFileName = sprintf('plan.%s.csv', date('Ymd-His'));
+        $logFileName = sprintf('plan.%s.log', date('Ymd-His'));
+
+        file_put_contents($csvFileName, $report);
+        file_put_contents($logFileName, join("", $logs));
+
+        return $plan;
+    }
+
+}
