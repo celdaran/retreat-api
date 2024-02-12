7ecbe469def4e01f433a1dafba8d23a193893074
Charlie Hills <github.cb25@tmx.email>
2024-02-12 00:07:35 -0600

    Fix issue where Simulator cannot be injected into Controller

--- src/Controller/Simulation.php
+++ src/Controller/Simulation.php
@@ -8,32 +8,38 @@ use App\Service\Engine\Simulator;
 
 class Simulation
 {
+    /** @var Simulator */
+    private Simulator $simulator;
+
+    public function __construct()
+    {
+        $this->simulator = new Simulator();
+    }
+
     /**
      * @param Request $request
-     * @param Simulator $simulator
      * @return JsonResponse
      * @noinspection PhpUnused
      */
     #[Route('/shortfalls', methods: ['POST'])]
-    public function shortfalls(Request $request, Simulator $simulator): JsonResponse
+    public function shortfalls(Request $request): JsonResponse
     {
-        $simulator->setParametersFromRequest($request);
-        $simulatorResponse = $simulator->runShortfalls();
+        $this->simulator->setParametersFromRequest($request);
+        $simulatorResponse = $this->simulator->runShortfalls();
         return new JsonResponse($simulatorResponse);
     }
 
     /**
      * @param Request $request
-     * @param Simulator $simulator
      * @return JsonResponse
      * @noinspection PhpUnused
      * @api
      */
     #[Route('/asset/depletion', methods: ['POST'])]
-    public function assetDepletion(Request $request, Simulator $simulator): JsonResponse
+    public function assetDepletion(Request $request): JsonResponse
     {
-        $simulator->setParametersFromRequest($request);
-        $simulatorResponse = $simulator->runAssetDepletion();
+        $this->simulator->setParametersFromRequest($request);
+        $simulatorResponse = $this->simulator->runAssetDepletion();
         return new JsonResponse($simulatorResponse);
     }
 }
