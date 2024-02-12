2db9ca8d4d35535d4b1f207abf12fc08f0905c85
Charlie Hills <github.cb25@tmx.email>
2024-02-11 22:34:34 -0600

    Refactor Simulation controller [m]
    The controller was way too heavy: contained too much business logic
    (which went from a theoretical problem to an actual problem when I
    went to add a Symfony command for running one of the endpoints.
    
    A new Service/Engine/Simulator class added to centralize the logic,
    most of which was pulled from the old controller.

--- src/Controller/Simulation.php
+++ src/Controller/Simulation.php
@@ -1,198 +1,39 @@
 <?php namespace App\Controller;
 
-use Exception;
-
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
-use App\Service\Engine\Engine;
-use App\Service\Engine\Until;
+use App\Service\Engine\Simulator;
 
 class Simulation
 {
     /**
      * @param Request $request
+     * @param Simulator $simulator
      * @return JsonResponse
+     * @noinspection PhpUnused
      */
-    #[Route('/summary', methods: ['POST'])]
-    public function summary(Request $request): JsonResponse
+    #[Route('/shortfalls', methods: ['POST'])]
+    public function shortfalls(Request $request, Simulator $simulator): JsonResponse
     {
-        $body = json_decode($request->getContent(), true);
-
-        $expense = $body['expense'];
-        $asset = $body['asset'];
-        $earnings = $body['earnings'];
-        $periods = $body['periods'];
-        $startYear = $body['startYear'];
-        $startMonth = $body['startMonth'];
-
-        $payload = [];
-
-        try {
-            $plan = $this->getSimulation($expense, $asset, $earnings, $periods, $startYear, $startMonth);
-            foreach ($plan->getSimulation() as $period) {
-                $payload[] = [
-                    "x" => sprintf("%04d-%02d", $period["year"], $period["month"]),
-                    "y" => $period["shortfall"],
-                ];
-            }
-        } catch (Exception $e) {
-            $payload = [
-                'code' => 500,
-                'message' => $e->getMessage(),
-            ];
-        }
-
-        return new JsonResponse($payload);
+        $simulator->setParametersFromRequest($request);
+        $simulatorResponse = $simulator->runShortfalls();
+        return new JsonResponse($simulatorResponse);
     }
 
     /**
      * @param Request $request
+     * @param Simulator $simulator
      * @return JsonResponse
+     * @noinspection PhpUnused
+     * @api
      */
     #[Route('/asset/depletion', methods: ['POST'])]
-    public function assetDepletion(Request $request): JsonResponse
+    public function assetDepletion(Request $request, Simulator $simulator): JsonResponse
     {
-        $body = json_decode($request->getContent(), true);
-
-        $expense = $body['expense'];
-        $asset = $body['asset'];
-        $earnings = $body['earnings'];
-        $periods = $body['periods'];
-        $startYear = $body['startYear'];
-        $startMonth = $body['startMonth'];
-
-        // Generate payload template
-        $payload = [
-            'dataset' => [
-                'source' => [
-                ],
-            ],
-            'tooltip' => [
-                "trigger" => "axis",
-                "axisPointer" => [
-                    "type" => "shadow"
-                ]
-            ],
-            'title' => [
-                "text" => "Asset Depletion",
-                "left" => "center",
-            ],
-            'legend' => [
-                "top" => 40,
-                "type" => "scroll",
-            ],
-            'grid' => [
-                "left" => 15,
-                "right" => 15,
-                "bottom" => 30,
-                "top" => 100,
-                "containLabel" => true
-            ],
-            'xAxis' => [
-                "type" => "category"
-            ],
-            'yAxis' => [
-                "type" => "value"
-            ],
-            'series' => [],
-        ];
-
-        $response = new Response();
-
-        // Fill in data source entries
-        try {
-            $assetList = [];
-            $first = true;
-
-            $response = $this->getSimulation($expense, $asset, $earnings, $periods, $startYear, $startMonth);
-            foreach ($response->getSimulation() as $period) {
-
-                $entry = [
-                    sprintf("%04d-%02d", $period["year"], $period["month"]),
-                ];
-
-                foreach ($period['assets'] as $name => $value) {
-                    $entry[] = $value;
-                    if ($first) {
-                        $assetList[] = $name;
-                    }
-                }
-
-                if ($first) {
-                    $header = ["period"];
-                    foreach ($assetList as $asset) {
-                        $header[] = $asset;
-                    }
-                    $payload['dataset']['source'][] = $header;
-                    $first = false;
-                }
-
-                $payload['dataset']['source'][] = $entry;
-            }
-
-            // Lastly define series
-            for ($i = 0; $i < count($assetList); $i++) {
-                $payload['series'][] = [
-                    "type" => "bar",
-                    "stack" => "Asset",
-                ];
-            }
-        } catch (Exception $e) {
-            $payload = [
-                'code' => 500,
-                'message' => $e->getMessage(),
-            ];
-        }
-
-        $response->setPayload($payload);
-
-        return new JsonResponse($response->getPayload());
+        $simulator->setParametersFromRequest($request);
+        $simulatorResponse = $simulator->runAssetDepletion();
+        return new JsonResponse($simulatorResponse);
     }
-
-    /**
-     * getPlan
-     *
-     * This method instantiates the engine, executes a plan, and returns the results.
-     *
-     * @param string $expense
-     * @param string $asset
-     * @param string $earnings
-     *
-     * @param string $periods
-     * @param string $startYear
-     * @param string $startMonth
-     *
-     * @return Response
-     * @throws Exception
-     */
-    private function getSimulation(
-        string $expense,
-        string $asset,
-        string $earnings,
-        string $periods,
-        string $startYear,
-        string $startMonth
-    ): Response {
-
-        $engine = new Engine(
-            $expense,
-            $asset,
-            $earnings
-        );
-        $until = new Until();
-        $until->setPeriods($periods);
-
-        if (!$engine->run($until, $startYear, $startMonth)) {
-            return new Response();
-        }
-
-        $simulation = $engine->getPlan();
-        $logs = $engine->getLogs();
-        $audit = $engine->getAudit();
-
-        return new Response(true, $simulation, $logs, $audit);
-    }
-
 }
---
+++ src/Service/Engine/Simulator.php
@@ -0,0 +1,211 @@
+<?php namespace App\Service\Engine;
+
+use Exception;
+use Symfony\Component\HttpFoundation\Request;
+use App\Engine\SimulatorResponse;
+
+class Simulator
+{
+    /** @var string */
+    private string $expense;
+
+    /** @var string */
+    private string $asset;
+
+    /** @var string */
+    private string $earnings;
+
+    /** @var int */
+    private int $periods;
+
+    /** @var int */
+    private int $startYear;
+
+    /** @var int */
+    private int $startMonth;
+
+    /**
+     * @return SimulatorResponse
+     */
+    public function runShortfalls(): SimulatorResponse
+    {
+        $payload = [];
+
+        try {
+            $response = $this->runSimulation();
+            foreach ($response->getSimulation() as $period) {
+                $payload[] = [
+                    "x" => sprintf("%04d-%02d", $period["year"], $period["month"]),
+                    "y" => $period["shortfall"],
+                ];
+            }
+        } catch (Exception $e) {
+            $response = new SimulatorResponse();
+            $payload = [
+                'code' => 500,
+                'message' => $e->getMessage(),
+            ];
+        }
+
+        $response->setPayload($payload);
+
+        return $response;
+    }
+
+    /**
+     * @return SimulatorResponse
+     */
+    public function runAssetDepletion(): SimulatorResponse
+    {
+        // Define payload template
+        $payload = [
+            'dataset' => [
+                'source' => [
+                ],
+            ],
+            'tooltip' => [
+                "trigger" => "axis",
+                "axisPointer" => [
+                    "type" => "shadow"
+                ]
+            ],
+            'title' => [
+                "text" => "Asset Depletion",
+                "left" => "center",
+            ],
+            'legend' => [
+                "top" => 40,
+                "type" => "scroll",
+            ],
+            'grid' => [
+                "left" => 15,
+                "right" => 15,
+                "bottom" => 30,
+                "top" => 100,
+                "containLabel" => true
+            ],
+            'xAxis' => [
+                "type" => "category"
+            ],
+            'yAxis' => [
+                "type" => "value"
+            ],
+            'series' => [],
+        ];
+
+        // Fill in data source entries
+        try {
+            $assetList = [];
+            $first = true;
+
+            $response = $this->runSimulation();
+            foreach ($response->getSimulation() as $period) {
+
+                $entry = [
+                    sprintf("%04d-%02d", $period["year"], $period["month"]),
+                ];
+
+                foreach ($period['assets'] as $name => $value) {
+                    $entry[] = $value;
+                    if ($first) {
+                        $assetList[] = $name;
+                    }
+                }
+
+                if ($first) {
+                    $header = ["period"];
+                    foreach ($assetList as $asset) {
+                        $header[] = $asset;
+                    }
+                    $payload['dataset']['source'][] = $header;
+                    $first = false;
+                }
+
+                $payload['dataset']['source'][] = $entry;
+            }
+
+            // Lastly define series
+            for ($i = 0; $i < count($assetList); $i++) {
+                $payload['series'][] = [
+                    "type" => "bar",
+                    "stack" => "Asset",
+                ];
+            }
+        } catch (Exception $e) {
+            $response = new SimulatorResponse();
+            $payload = [
+                'code' => 500,
+                'message' => $e->getMessage(),
+            ];
+        }
+
+        $response->setPayload($payload);
+
+        return $response;
+    }
+
+    /**
+     * @param Request $request
+     */
+    public function setParametersFromRequest(Request $request)
+    {
+        $body = json_decode($request->getContent(), true);
+        $this->setParameters(
+            $body['expense'],
+            $body['asset'],
+            $body['earnings'],
+            (int)$body['periods'],
+            (int)$body['startYear'],
+            (int)$body['startMonth'],
+        );
+    }
+
+    /**
+     * @param string $expense
+     * @param string $asset
+     * @param string $earnings
+     * @param int $periods
+     * @param int $startYear
+     * @param int $startMonth
+     */
+    public function setParameters(
+        string $expense,
+        string $asset,
+        string $earnings,
+        int $periods,
+        int $startYear,
+        int $startMonth)
+    {
+        $this->expense = $expense;
+        $this->asset = $asset;
+        $this->earnings = $earnings;
+        $this->periods = $periods;
+        $this->startYear = $startYear;
+        $this->startMonth = $startMonth;
+    }
+
+    /**
+     * runSimulation
+     * This method instantiates the engine, runs a simulation, and returns the results.
+     * @return SimulatorResponse
+     * @throws Exception
+     */
+    private function runSimulation(): SimulatorResponse
+    {
+        $engine = new Engine(
+            $this->expense,
+            $this->asset,
+            $this->earnings
+        );
+        $until = new Until();
+        $until->setPeriods($this->periods);
+
+        $success = $engine->run($until, $this->startYear, $this->startMonth);
+        $simulation = $engine->getSimulation();
+        $logs = $engine->getLogs();
+        $audit = $engine->getAudit();
+
+        return new SimulatorResponse($success, $simulation, $logs, $audit);
+    }
+
+}
