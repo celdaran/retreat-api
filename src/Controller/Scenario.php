930ffb7d23ac7d4b6417b295b1fd7140bf32f962
Charlie Hills <github.cb25@tmx.email>
2024-02-12 00:09:07 -0600

    Convert clone from Restler to Symfony
    I neglected to pick this one up on the last pass.

--- src/Controller/Scenario.php
+++ src/Controller/Scenario.php
@@ -1,23 +1,32 @@
 <?php namespace App\Controller;
 
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
 use App\System\LogFactory;
 use App\Service\Scenario\Scenario as DataScenario;
 
 class Scenario
 {
     /**
-     * @url POST /clone
-     * @param string $newScenarioName
-     * @param string $newScenarioDescr
-     * @param string $oldScenarioId
-     * @return array
+     * @param Request $request
+     * @return JsonResponse
+     * @noinspection PhpUnused
      */
-    public function clone(string $newScenarioName, string $newScenarioDescr, string $oldScenarioId): array
+    #[Route('/clone', methods: ['POST'])]
+    public function clone(Request $request): JsonResponse
     {
+        $body = json_decode($request->getContent(), true);
+
+        $newScenarioName = $body['newScenarioName'];
+        $newScenarioDescr = $body['newScenarioDescr'];
+        $oldScenarioId = $body['oldScenarioId'];
+
         $log = LogFactory::getLogger();
         $scenario = new DataScenario($log);
         $scenario->clone($oldScenarioId, $newScenarioName, $newScenarioDescr, 1);
-        return ['msg' => 'Probably succeeded. IDK'];
+        return new JsonResponse(['msg' => 'Probably succeeded...idk']);
     }
 
 }
